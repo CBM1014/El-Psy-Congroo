86b386017438969cec13d085e847a1c40eeb1a5f
CBM1014 <qlx1014@gmail.com>
2018-12-14 21:00:39 +0800

    Add an interface about LanguageManager  Changes to be committed: 	modified:   src/main/java/cat/melon/El_Psy_Congroo/Init.java 	new file:   src/main/java/cat/melon/El_Psy_Congroo/LanguageManager.java

--- src/main/java/cat/melon/El_Psy_Congroo/Init.java
+++ src/main/java/cat/melon/El_Psy_Congroo/Init.java
@@ -1,6 +1,21 @@
 package cat.melon.El_Psy_Congroo;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Map;
+
 public class Init extends JavaPlugin {
+    LanguageManager languageManager;
+    FileConfiguration config;
+
+    @Override
+    public void onEnable(){
+        Long timestart = System.currentTimeMillis();
+        config = this.getConfig();
+        languageManager = new LanguageManager(this.config.getString("language"));
+        //setup plugin
+        this.getLogger().info(languageManager.getLang("plugin.load.completed",(System.currentTimeMillis()-timestart)/1000));
+    }
 }
---
+++ src/main/java/cat/melon/El_Psy_Congroo/LanguageManager.java
@@ -0,0 +1,32 @@
+package cat.melon.El_Psy_Congroo;
+
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+
+import java.io.File;
+
+public class LanguageManager {
+    FileConfiguration languageconfig;
+
+    public LanguageManager() {
+    }
+
+    public LanguageManager(String language) {
+    }
+
+    public String getLang(String langPath) {
+        return "";
+    }
+
+    public String getLang(String langPath, Object placeHolder0) {
+        return "";
+    }
+
+    public String getLang(String langPath, Object placeHolder0, Object placeHolder1) {
+        return "";
+    }
+
+    public String getLang(String langPath, Object placeHolder0, Object placeHolder1, Object placeHolder2) {
+        return "";
+    }
+}
