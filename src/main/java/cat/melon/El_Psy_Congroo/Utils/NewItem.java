a3a6bd3c2ce8441a082b2736717c38b1b5cb6913
CBM1014 <qlx1014@gmail.com>
2018-12-31 12:19:46 +0800

    add a unique NBT tag on NewItems  Changes to be committed: 	modified:   pom.xml 	modified:   src/main/java/cat/melon/El_Psy_Congroo/Utils/NewItem.java

--- src/main/java/cat/melon/El_Psy_Congroo/Utils/NewItem.java
+++ src/main/java/cat/melon/El_Psy_Congroo/Utils/NewItem.java
@@ -1,45 +1,54 @@
 package cat.melon.El_Psy_Congroo.Utils;
 
 import cat.melon.El_Psy_Congroo.Init;
+import de.tr7zw.itemnbtapi.NBTItem;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
-import sun.plugin2.main.server.Plugin;
 
 import java.util.List;
 
 public class NewItem implements Listener {
     private Material type;
     private short damage;
-    private String displayname;
+    private String displayName;
     private List<String> lore;
 
-    public NewItem(Init instance,Material type, String displayname, short damage, List<String> lore) {
+    public NewItem(Init instance, Material type, String displayname, short damage, List<String> lore) {
         this.type = type;
         this.damage = damage;
-        this.displayname = displayname;
+        this.displayName = displayname;
         this.lore = lore;
-        Bukkit.getServer().getPluginManager().registerEvents(this,instance);
+        Bukkit.getServer().getPluginManager().registerEvents(this, instance);
     }
 
-    public ItemStack getItem(int amount){
-        ItemStack tmpis = new ItemStack(type,amount,damage);
+    public ItemStack getItem(int amount) {
+        ItemStack tmpis = new ItemStack(type, amount, damage);
         tmpis.setLore(lore);
-        tmpis.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        tmpis.addItemFlags(ItemFlag.HIDE_ENCHANTS, ItemFlag.HIDE_UNBREAKABLE, ItemFlag.HIDE_ATTRIBUTES);
+        tmpis.getItemMeta().setUnbreakable(true);
         tmpis.getItemMeta().setDisplayName("");
         //TODO set item name here
-        return tmpis;
-    }
-
-    public int getDamage() {
-        return damage;
+        NBTItem tmpni = new NBTItem(tmpis);
+        tmpni.setBoolean("ElNewItem", true);
+        return tmpni.getItem();
     }
 
     public Material getType() {
         return type;
+    }
+
+    public short getDamage() {
+        return damage;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public List<String> getLore() {
+        return lore;
     }
 }
