5a5377d76ef93d10ad01e50a9c3119b352e61eed
Sotr <i@omc.hk>
2019-01-02 18:06:06 +0800

    Add useful helpers

--- src/main/java/cat/melon/El_Psy_Congroo/LanguageManager.java
+++ src/main/java/cat/melon/El_Psy_Congroo/LanguageManager.java
@@ -2,9 +2,12 @@ package cat.melon.El_Psy_Congroo;
 
 import org.apache.commons.lang3.StringUtils;
 import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import cat.melon.El_Psy_Congroo.Utils.lib.LocalizationAPI;
 import cat.melon.El_Psy_Congroo.Utils.lib.LocalizationAPI.Opcodes;
+import moe.kira.personal.PersonalAPI;
 
 public class LanguageManager {
     private static final String BEDROCK_LOCALE = "en_UK";
@@ -25,6 +28,10 @@ public class LanguageManager {
     public String getLang(String languagePath, Object... placeHolders) {
         return getLang(languagePath, defaultLang, placeHolders);
     }
+    
+    public String getLang(String languagePath, CommandSender player, Object... placeHolders) {
+        return getLang(languagePath, PersonalAPI.of(player).getString("el_psy_congroo.locale"), placeHolders);
+    }
 
     //return texts with placeholders
     public String getLang(String languagePath, String lang, Object... placeHolders) {
@@ -37,6 +44,10 @@ public class LanguageManager {
 
     public String getLang(String languagePath) {
         return getLang(languagePath, defaultLang);
+    }
+    
+    public String getLang(String languagePath, CommandSender player) {
+        return getLang(languagePath, PersonalAPI.of(player).getString("el_psy_congroo.locale"));
     }
     
     //if no placeholders in texts, using this method to skip the placeholder loop
