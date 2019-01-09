fbfa5cbc93c224dc4ba4400ef71d0b3ab9e58690
CBM1014 <qlx1014@gmail.com>
2019-01-09 16:03:38 +0800

    add the structure of player status system  Changes to be committed: 	new file:   src/main/java/cat/melon/El_Psy_Congroo/Player/Player.java 	new file:   src/main/java/cat/melon/El_Psy_Congroo/Player/Status.java 	new file:   src/main/java/cat/melon/El_Psy_Congroo/ScheduledTasks/EnviromentCamera.java 	new file:   src/main/java/cat/melon/El_Psy_Congroo/StatusManager.java 	modified:   src/main/java/cat/melon/El_Psy_Congroo/SubtitleManager.java 	deleted:    src/main/java/cat/melon/El_Psy_Congroo/Utils/Player.java

---
+++ src/main/java/cat/melon/El_Psy_Congroo/StatusManager.java
@@ -0,0 +1,12 @@
+package cat.melon.El_Psy_Congroo;
+
+import cat.melon.El_Psy_Congroo.Player.Player;
+import cat.melon.El_Psy_Congroo.ScheduledTasks.EnviromentCamera;
+import org.bukkit.event.Listener;
+
+import java.util.ArrayList;
+
+public class StatusManager implements Listener {
+    private ArrayList<Player> playerArrayList = new ArrayList<>();
+
+}
