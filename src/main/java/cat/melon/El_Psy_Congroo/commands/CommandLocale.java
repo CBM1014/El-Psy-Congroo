814de3ea83fa64a207b767c454aa52395948dac4
Sotr <i@omc.hk>
2019-02-14 18:49:05 +0800

    Use new api

--- src/main/java/cat/melon/El_Psy_Congroo/commands/CommandLocale.java
+++ src/main/java/cat/melon/El_Psy_Congroo/commands/CommandLocale.java
@@ -1,10 +1,11 @@
 package cat.melon.el_psy_congroo.commands;
 
-import org.bukkit.Sound;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 
 import moe.kira.personal.PersonalAPI;
+import moe.kira.poi.Poi;
 
 public class CommandLocale implements Command, PlayerOnly {
     @Override
@@ -12,8 +13,9 @@ public class CommandLocale implements Command, PlayerOnly {
         if (labels.length == 0)
             return;
         // Update locale setting
-        PersonalAPI.of(sender).set("el_psy_congroo.locale", labels[0]);
+        YamlConfiguration personal = PersonalAPI.of(sender);
+        personal.set("el_psy_congroo.locale", labels[0]);
         // Notify by playing sound
-        ((Player) sender).playSound(((Player) sender).getEyeLocation(), Sound.BLOCK_END_PORTAL_FRAME_FILL, 10F, 1F);
+        ((Player) sender).playSound(((Player) sender).getEyeLocation(), Poi.get(personal).secretCode(), 10F, 1F);
     }
 }
